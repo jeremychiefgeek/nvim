404d960cf97f89773b6600ab01728f1acabb6429
Jeremy Evans <jeremy@chiefgeek.io>
2024-02-09 17:00:52 -0500

    Updated treesitter and telescope

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,21 @@
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup({
 	-- A list of parser names, or "all" (the five listed parsers should always be installed)
-	ensure_installed = { "javascript", "typescript", "c_sharp", "vue", "hlsl", "glsl", "cpp", "c", "lua", "rust", "zig", "cmake", "odin" },
+	ensure_installed = {
+		"javascript",
+		"typescript",
+		"c_sharp",
+		"vue",
+		"hlsl",
+		"glsl",
+		"cpp",
+		"c",
+		"lua",
+		"rust",
+		"zig",
+		"cmake",
+		"odin",
+		"jsdoc",
+	},
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
@@ -12,7 +27,6 @@ require'nvim-treesitter.configs'.setup {
 	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
 	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-
 	highlight = {
 		enable = true,
 		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
@@ -21,6 +35,6 @@ require'nvim-treesitter.configs'.setup {
 		-- Instead of true it can also be a list of languages
 		additional_vim_regex_highlighting = false,
 	},
-}
-require 'nvim-treesitter.install'.prefer_git = false
-require 'nvim-treesitter.install'.compilers = { "gcc" }
+})
+require("nvim-treesitter.install").prefer_git = false
+require("nvim-treesitter.install").compilers = { "gcc" }
--- lua/chiefgeek/packer.lua
+++ lua/chiefgeek/packer.lua
@@ -1,118 +1,116 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or, branch = '0.1.x',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    } 
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		-- or, branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
---    use({
---        'rose-pine/neovim',
---        as = 'rose-pine',
---        config = function()
---            vim.cmd('colorscheme rose-pine')
---        end
---    })
--- Dev Begin
--- Dev End
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("mbbill/undotree")
+	use("tpope/vim-fugitive")
+	use("folke/trouble.nvim")
+	use({
+		"p00f/clangd_extensions.nvim",
+		config = function()
+			local clangd_extensions = require("clangd_extensions")
+			clangd_extensions.setup()
+		end,
+	})
 
--- Trying these plugins Begin
--- Trying these plugins End
+	-- Theme
+	use("shaunsingh/nord.nvim")
+	-- Theme End
 
-use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
-use('nvim-treesitter/playground')
-use('theprimeagen/harpoon')
-use('mbbill/undotree')
-use('tpope/vim-fugitive')
-use('folke/trouble.nvim')
-use {'p00f/clangd_extensions.nvim', config = function() 
- local clangd_extensions =  require("clangd_extensions")
- clangd_extensions.setup()
- end
-}
---use('navarasu/onedark.nvim')
---use {'Mofiqul/dracula.nvim'}--, config = function() vim.cmd.colorscheme("gruvbox") end }
-use 'shaunsingh/nord.nvim'
-use ("nvim-lua/plenary.nvim")
-use {'folke/todo-comments.nvim', config = function()
- local todo_comments = require('todo-comments')
- todo_comments.setup()
- end
-}
+	use("nvim-lua/plenary.nvim")
+	use({
+		"folke/todo-comments.nvim",
+		config = function()
+			local todo_comments = require("todo-comments")
+			todo_comments.setup()
+		end,
+	})
 
-use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-        -- LSP Support
-        {'neovim/nvim-lspconfig'},             -- Required
-        {                                      -- Optional
-        'williamboman/mason.nvim',
-        run = function()
-            pcall(vim.cmd, 'MasonUpdate')
-        end,
-    },
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use({
+		"kdheepak/lazygit.nvim",
+		-- optional for floating window border decoration
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
-    {'mhartington/formatter.nvim'},
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ -- Optional
+				"williamboman/mason.nvim",
+				run = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
+			{ "mhartington/formatter.nvim" },
 
-    {'neoclide/coc.nvim', branch = 'release'},
-}
-}
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
 
-use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-}
+			{ "neoclide/coc.nvim", branch = "release" },
+		},
+	})
 
-  use {
-    'MarcHamamji/runner.nvim',
-    requires = {
-      'nvim-telescope/telescope.nvim',
-      requires = { 'nvim-lua/plenary.nvim' }
-    },
-    config = function()
-        require('runner').setup()
-    end
-  }
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
 
+	use({
+		"MarcHamamji/runner.nvim",
+		requires = {
+			"nvim-telescope/telescope.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
+		},
+		config = function()
+			require("runner").setup()
+		end,
+	})
 
+	use({
+		"alexghergh/nvim-tmux-navigation",
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
 
-use { 'alexghergh/nvim-tmux-navigation', config = function()
+			nvim_tmux_nav.setup({
+				disable_when_zoomed = true, -- defaults to false
+			})
 
-        local nvim_tmux_nav = require('nvim-tmux-navigation')
-
-        nvim_tmux_nav.setup {
-            disable_when_zoomed = true -- defaults to false
-        }
-
-        vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
-        vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
-        vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
-        vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
-        vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
-        vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
-
-    end
-}
-
+			vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+			vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+			vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+			vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+			vim.keymap.set("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+		end,
+	})
 end)
-
