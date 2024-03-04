5b901b09ec309ce619a8175354aac999095094ec
Jeremy Evans <jeremy@chiefgeek.io>
2024-03-04 09:22:18 -0500

    Added neorg

--- lua/chiefgeek/packer.lua
+++ lua/chiefgeek/packer.lua
@@ -113,4 +113,24 @@ return require("packer").startup(function(use)
 			vim.keymap.set("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
 		end,
 	})
+	use({
+		"nvim-neorg/neorg",
+		config = function()
+			require("neorg").setup({
+				load = {
+					["core.defaults"] = {}, -- Loads default behaviour
+					["core.concealer"] = {}, -- Adds pretty icons to your documents
+					["core.dirman"] = { -- Manages Neorg workspaces
+						config = {
+							workspaces = {
+								notes = "~/notes",
+							},
+						},
+					},
+				},
+			})
+		end,
+		run = ":Neorg sync-parsers",
+		requires = "nvim-lua/plenary.nvim",
+	})
 end)
